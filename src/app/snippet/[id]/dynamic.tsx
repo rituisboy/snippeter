5615f1f62fb622e5cb32330cc81684c0d251cb5a
rituisboy <riturajthakur100@gmail.com>
2024-07-27 12:26:30 +0530

    _

--- src/app/snippet/[id]/dynamic.tsx
+++ src/app/snippet/[id]/dynamic.tsx
@@ -1,30 +1,30 @@
-import React, { useEffect, useState } from "react";
-import AceEditor from "react-ace";
+// import React, { useEffect, useState } from "react";
+// import AceEditor from "react-ace";
 
-function DynamicAceEditor({ mode, theme }) {
-  const [loaded, setLoaded] = useState(false);
+// function DynamicAceEditor({ mode, theme }) {
+//   const [loaded, setLoaded] = useState(false);
 
-  useEffect(() => {
-    async function loadDependencies() {
-      await import(`ace-builds/src-noconflict/mode-${mode}`);
-      await import(`ace-builds/src-noconflict/theme-${theme}`);
-      setLoaded(true);
-    }
+//   useEffect(() => {
+//     async function loadDependencies() {
+//       await import(`ace-builds/src-noconflict/mode-${mode}`);
+//       await import(`ace-builds/src-noconflict/theme-${theme}`);
+//       setLoaded(true);
+//     }
 
-    loadDependencies();
-  }, [mode, theme]);
+//     loadDependencies();
+//   }, [mode, theme]);
 
-  if (!loaded) {
-    return <div>Loading...</div>;
-  }
+//   if (!loaded) {
+//     return <div>Loading...</div>;
+//   }
 
-  return (
-    <AceEditor
-      mode={mode}
-      theme={theme}
-      // ... other props
-    />
-  );
-}
+//   return (
+//     <AceEditor
+//       mode={mode}
+//       theme={theme}
+//       // ... other props
+//     />
+//   );
+// }
 
-export default DynamicAceEditor;
+// export default DynamicAceEditor;
